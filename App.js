96da6d55901024ecf6a344a220344fa3e7b9483b
anujbidkar <anujbidkar8@gmail.com>
2020-07-06 22:27:26 +0530

    Full Game

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View,Text,StyleSheet,Image,TouchableOpacity,Alert} from "react-native";
 
-export default function App() {
-  return (
+export default class App extends React.Component{
+  myButton = ()=>{
+    var numBer = this.getRandomNumber();
+    switch (numBer)
+    {
+      case 1:
+        this.setState({
+          uri:require('./assets/dice1.png')})
+          break;
+      case 2:
+        this.setState({
+          uri:require('./assets/dice2.png')})
+          break;
+      case 3:
+        this.setState({
+          uri:require('./assets/dice3.png')})
+          break;
+      case 4:
+        this.setState({
+          uri:require('./assets/dice4.png')})
+          break;
+      case 5:
+        this.setState({
+          uri:require('./assets/dice5.png')})
+          break;
+      case 6:
+        this.setState({
+          uri:require('./assets/dice6.png')})
+          break;
+      default:
+        Alert.alert("hello");
+
+
+    }
+  
+  }
+  getRandomNumber= ()=>{
+    return Math.floor(Math.random()*6)+1;
+
+   }
+  constructor(props){
+    super(props);
+    this.state =
+    {
+                uri:require('./assets/dice1.png')
+    }
+    };
+
+   
+
+  render(){
+
+    return( 
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={this.state.uri}/>
+      <TouchableOpacity
+        onPress={this.myButton}
+      >
+      <Text style={styles.button}>Play Game !</Text>
+      </TouchableOpacity>
     </View>
-  );
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center",
+    fontSize:60,
+    backgroundColor:"#d291bc"
   },
-});
+  button:{
+    fontSize:30,
+    marginTop:20,
+    color:"white",
+    fontWeight:"bold",
+    borderWidth:2,
+    padding:20,
+    borderRadius:10
+
+  }
+})
